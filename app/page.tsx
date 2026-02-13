dc6e07c898e01e492bdcc0cd6fdd431161d67ca9
okorikokob <okorikokob@gmail.com>
2026-02-13 23:51:17 +0100

    assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 const page = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <h1 className="text-danger-500 text-heading-xl">Hello world!</h1>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
