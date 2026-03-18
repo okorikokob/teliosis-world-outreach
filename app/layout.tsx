587ca48a0177a6d695513fa20d9ec0942e51e241
okorikokob <okorikokob@gmail.com>
2026-03-19 00:04:14 +0100

    chore: setup advanced global seo metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,12 +10,47 @@ const inter = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Teliosis World Outreach",
+  title: {
+    template: "%s | Teliosis World Outreach",
+    default: "Teliosis World Outreach",
+  },
+
   description:
     "A vibrant community making disciples who love God, love people, and transform the world through the Gospel. Experience life-changing worship and discover your God-given purpose.",
-  icons: "/images/logo.png",
-};
 
+  icons: {
+    icon: "/assets/logo.png",
+  },
+  keywords: ["Church", "Teliosis World Outreach", "Christian", "Sermons", "Devotionals", "Faith", "Worship"],
+  openGraph: {
+    title: "Teliosis World Outreach",
+    description:
+      "A vibrant community making disciples who love God, love people, and transform the world through the Gospel.",
+    url: "https://www.teliosis.org",
+    siteName: "Teliosis World Outreach",
+    images: [
+      {
+        url: "/assets/logo.png",
+        width: 1200,
+        height: 630,
+        alt: "Teliosis World Outreach Logo",
+      },
+    ],
+    locale: "en_NG",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Teliosis World Outreach",
+    description:
+      "A vibrant community making disciples who love God, love people, and transform the world through the Gospel.",
+    images: ["/assets/logo.png"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
 export default function RootLayout({
   children,
 }: Readonly<{
